3348a0b6ae4a3bc6a3e2697de7cc7b7616a24215
mmills2 <34921905-mmills2@users.noreply.replit.com>
2024-11-08 04:49:38 +0000

    Checkpoint before revert - Implement comparative analysis tools

--- replit.nix
+++ replit.nix
@@ -1,5 +1,15 @@
 {pkgs}: {
   deps = [
+    pkgs.glibcLocales
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.freetype
+    pkgs.ffmpeg-full
+    pkgs.cairo
     pkgs.xsimd
     pkgs.pkg-config
     pkgs.libxcrypt
